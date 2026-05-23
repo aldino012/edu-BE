041706ec83580f8abdf370291a84f82d84a0dd2a
aldino012 <yozukakit@gmail.com>
2026-05-23 09:11:17 +0700

    Update Server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://edu-fe-apa-gitu.vercel.app",
+      "https://edu-fe.vercel.app",
       "http://127.0.0.1:5173",
       "http://localhost:3000",
       "http://localhost:8080", // Tambahan izin untuk Nginx/Docker Frontend
